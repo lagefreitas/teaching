e19bbe6662143af3f426fe2b4b420b8c5bc697a4
Júlio <diolandajr@gmail.com>
2015-10-18 21:03:39 -0300

    Alguns comentários em inglês adicionados a main.c e plot.jl

--- 2015.1-IAC-trabalho-pratico/src/main.c
+++ 2015.1-IAC-trabalho-pratico/src/main.c
@@ -7,7 +7,7 @@
 #include <sys/sysinfo.h>
 #include <pthread.h>
 
-#define NUCLEOS get_nprocs_conf()
+#define NUCLEOS get_nprocs_conf() //Total CPU cores
 
 //Global general purpose iterator
 int i;
@@ -22,11 +22,12 @@ void *consumeCPU_thread(void *threadid)
 }
 
 void consumeMemory() {
+	//Thread to the continuous CPU usage
 	pthread_t thread;
 	pthread_create(&thread, NULL, consumeCPU_thread, (void *)1);
 
 	for(;;){
-		malloc(1024);
+		malloc(1024); //Each loop allocates 1024 bytes in memory
 		usleep(100);
 	}
 }
@@ -34,25 +35,27 @@ void consumeMemory() {
 //CPU: ps -p <PID> -o pcpu | sed 1d | tr -d ' '
 double getCpuUsage(int pid){
 	char comando[50];
-	FILE *fp; //Ponteiro para a leitura do arquivo retornado pelo popen
+	FILE *fp; //Pointer to the file reading returned by popen
 	double porcentagem;
 
 	sprintf(comando, "%s%d%s", "ps -p ", pid, " -o pcpu | sed 1d | tr -d ' '");
 	fp = popen(comando, "r");
 	fscanf(fp, "%lf", &porcentagem);
+	fclose(fp);
 
 	return porcentagem/NUCLEOS;
 }
 
-//Memória: pmap <PID> -x | grep total | awk '{print $4}'
+//Memory: pmap <PID> -x | grep total | awk '{print $4}'
 int getMemoryUsage(int pid){
 	char comando[50];
-	FILE *fp; //Ponteiro para a leitura do arquivo retornado pelo popen
+	FILE *fp; //Pointer to the file reading returned by popen
 	int memoria;
 
 	sprintf(comando, "%s%d%s", "pmap ", pid, " -x | grep total | awk '{print $4}'");
 	fp = popen(comando, "r");
 	fscanf(fp, "%d", &memoria);
+	fclose(fp);
 
 	return memoria;
 }
@@ -71,13 +74,13 @@ int main (int argc, char *argv[], char *envp[]) {
 		struct sysinfo info;
 		sysinfo( &info );
 
-		FILE *logFile, *plotFile;
-		logFile = fopen("log.txt", "w");
-		plotFile = fopen("plot.txt", "w");
+		FILE *logFile, *plotFile; //Pointers to log.txt and plot.txt files
+		logFile = fopen("log.txt", "w"); //Opening log.txt with write permission
+		plotFile = fopen("plot.txt", "w"); //Opening plot.txt with write permission
 
-		printf("PID: %d\n", pid);
+		printf("PID: %d\n", pid); //PID of the child process
 		printf("Número de núcleos: %d\n", NUCLEOS);
-		printf("Memória total: %lu MB\n\n", ((size_t)info.totalram * (size_t)info.mem_unit)/(1024*1024));
+		printf("Memória total: %lu MB\n\n", ((size_t)info.totalram * (size_t)info.mem_unit)/(1024*1024)); //Total primary memory
 
 		printf("Tempo\tCPU\tMemória\n");
 
@@ -85,13 +88,14 @@ int main (int argc, char *argv[], char *envp[]) {
 			double cpuUsage = getCpuUsage(pid);
 			int memoryUsage = getMemoryUsage(pid);
 			printf("%ds\t%.2lf%%\t%d kB\n", i, cpuUsage, memoryUsage);
-			fprintf(logFile, "%ds\t%.2lf%%\t%d kB\n", i, cpuUsage, memoryUsage);
-			fprintf(plotFile, "%d %.2lf %d \n", i, cpuUsage, memoryUsage);
+			fprintf(logFile, "%ds\t%.2lf%%\t%d kB\n", i, cpuUsage, memoryUsage); //Writing the data in log.txt
+			fprintf(plotFile, "%d %.2lf %d \n", i, cpuUsage, memoryUsage); //Writing the data in plot.txt
 			sleep(1);
 		}
+		//Closing the opened files
 		fclose(logFile);
 		fclose(plotFile);
-		kill(pid, SIGKILL); //Mata o processo filho
+		kill(pid, SIGKILL); //Kill the child process
 	}
 	else {
 		if (strcmp(argv[1], "cpu") == 0 ) {
@@ -108,6 +112,6 @@ int main (int argc, char *argv[], char *envp[]) {
 		system("julia plot.jl");
 	}
 
-	exit(0); /* encerra o processo com sucesso (código 0) */
+	exit(0); /* Terminates the process successfully (code 0) */
 
 }
